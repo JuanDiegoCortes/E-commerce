ced80cbe384fe688bd7012cc63c782a64542bf42
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-13 22:36:11 -0500

    OrdenService

--- E-commerce/src/main/java/com/Ecommerce/service/IOrdenService.java
+++ E-commerce/src/main/java/com/Ecommerce/service/IOrdenService.java
@@ -1,5 +1,16 @@
 package com.Ecommerce.service;
 
+import com.Ecommerce.model.EnvioModel;
+import com.Ecommerce.model.OrdenModel;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IOrdenService {
     //funcionalidades /logica que  realizara esta entidad
+    String crearOrden(OrdenModel orden);
+    List<OrdenModel> listarOrdenes();
+    Optional<OrdenModel> obtenerOrdenPorId(int ordenId);
+    String eliminarOrdenPorId(int ordenId);
+    String actualizarOrdenPorId(OrdenModel orden);
 }
