8537f4ff47f524ba4bc446d7497063ef3dc89b0a
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-16 20:21:13 -0500

    Crear de producto
    No toquen esa vaina ta' delicada.

--- E-commerce/src/main/java/com/Ecommerce/controller/ProductoController.java
+++ E-commerce/src/main/java/com/Ecommerce/controller/ProductoController.java
@@ -1,11 +1,16 @@
 package com.Ecommerce.controller;
 
+import com.Ecommerce.domain.ProductoDTO;
 import com.Ecommerce.exception.CamposInvalidosException;
 import com.Ecommerce.exception.RecursoNoEncontradoException;
 import com.Ecommerce.model.CategoriaModel;
+import com.Ecommerce.model.ProdTallaModel;
 import com.Ecommerce.model.ProductoModel;
+import com.Ecommerce.model.TallaModel;
 import com.Ecommerce.service.ICategoriaService;
+import com.Ecommerce.service.IProdTallaService;
 import com.Ecommerce.service.IProductoService;
+import com.Ecommerce.service.ITallaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -22,20 +28,67 @@ public class ProductoController {
     private IProductoService productoService;
     @Autowired
     private ICategoriaService categoriaService;
+    @Autowired
+    private ITallaService tallaService;
+    @Autowired
+    private IProdTallaService prodTallaService;
 
     @PostMapping("/")
-    public ResponseEntity<String> crearProducto(@RequestBody ProductoModel producto) {
+    public ResponseEntity<String> crearProducto(@RequestBody ProductoDTO productoDTO) {
         //Verificar si el producto ya existe
-        CategoriaModel categoria = categoriaService.obtenerCategoriaPorId(producto.getIdCategoria().getIdCategoria())
-                .orElseThrow(()-> new RecursoNoEncontradoException("La categoria no existe."));
+        boolean bandera = true;
 
-        Optional<ProductoModel> verificacion = productoService.obtenerProductoPorId(producto.getIdProducto());
+        Optional<ProductoModel> verificacion = productoService.obtenerProductoPorId(productoDTO.getIdProducto());
         if (verificacion.isPresent()){
             String mensaje = "Este producto ya existe.";
             return new ResponseEntity<String>(mensaje, HttpStatus.BAD_REQUEST);
         }
-        productoService.crearProducto(producto);
-        return new ResponseEntity<String>(productoService.crearProducto(producto), HttpStatus.OK);
+
+        CategoriaModel categoria = categoriaService.obtenerCategoriaPorId(productoDTO.getIdCategoria().getIdCategoria())
+                .orElseThrow(()-> new RecursoNoEncontradoException("La categoria no existe."));
+
+        //Crear instancia de un productoModel
+        ProductoModel producto = new ProductoModel();
+        producto.setNombre(productoDTO.getNombre());
+        producto.setDescripcion(productoDTO.getDescripcion());
+        producto.setPrecio(productoDTO.getPrecio());
+        producto.setIsActive(productoDTO.getIsActive());
+        producto.setImage_Url(productoDTO.getImage_Url());
+        producto.setTipoProducto(productoDTO.getTipoProducto());
+        producto.setIdCategoria(categoria);
+
+        //Capturar las tallas y verificar que existan
+        if (productoDTO.getTallas() != null) {
+            List<Map<String, Integer>> listarTallas = productoDTO.getTallas();
+            for (Map<String, Integer> Tallas : listarTallas) {
+                Integer idTalla = Tallas.get("idTalla");
+                TallaModel talla = this.tallaService.obtenerTallaPorId(idTalla)
+                        .orElseThrow(() -> new RecursoNoEncontradoException("No esta la talla con id: " + idTalla));
+                if (talla == null) {
+                    bandera = false;
+                    throw new RecursoNoEncontradoException("No se encontró la talla con ID: " + idTalla);
+                }
+            }
+            if (bandera) {
+                System.out.println("Bandera: "+ bandera);
+                productoService.crearProducto(producto);
+                for (Map<String, Integer> Tallas : listarTallas) {
+                    Integer idTalla = Tallas.get("idTalla");
+                    ProdTallaModel prodTalla = new ProdTallaModel();
+                    TallaModel talla = tallaService.obtenerTallaPorId(idTalla).get();
+                    prodTalla.setIdProducto(producto);
+                    prodTalla.setIdTalla(talla);
+                    this.prodTallaService.crearProdTalla(prodTalla);
+                }
+                return new ResponseEntity<String>(productoService.crearProducto(producto), HttpStatus.OK);
+            } else{
+                String mensaje = "Verifica que los datos ingresados sean correctos.";
+                return new ResponseEntity<>(mensaje, HttpStatus.BAD_REQUEST); //Envia esto cuando no existen las tallas
+            }
+        }else{
+            String mensaje = "Verifica que hayas ingresado las tallas del producto.";
+            return new ResponseEntity<>(mensaje, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/")
