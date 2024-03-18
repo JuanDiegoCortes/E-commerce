8d0f190411d605991b4fcaa051cacf0cf9e63727
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-18 09:08:57 -0500

    Creando la apiweb
    Le puse apiweb para que no la confundamos directamente con el nombre del proyecto que seria E-commerce

---
+++ server/Apiweb/src/test/java/com/example/Apiweb/ApiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
