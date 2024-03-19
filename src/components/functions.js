3f881fef718f61ecaa6c4e3c1406d8d5d38e2382
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-18 21:53:48 -0500

    Algunas mejoras

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,98 +1,106 @@
-//Metodo para obtener los productos de la base de datos
-fetch('http://localhost:8080/Apiweb/v1/producto/')
-  .then(response => response.json())
-  .then(datos => {
-    const productGrid = document.querySelector('.product-grid');
-    datos.forEach(producto => {
-      const productoElement = document.createElement('div');
-      productoElement.classList.add('producto');
-      productoElement.innerHTML = `
-        <img src="${producto.image_Url}" alt="${producto.nombre}">
-        <h3>${producto.nombre}</h3>
-        <p>${producto.descripcion}</p>
-        <p>Precio: ${producto.precio}</p>
-      `;
-      productGrid.appendChild(productoElement);
-    });
-  })
-  .catch(error => console.error('Error:', error));
-
-//Metodo para crear un producto para la base de datos
-function crearProducto(productoDTO) {
-  fetch('http://localhost:8080/Apiweb/v1/producto/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+const productoController = {
+    mostrarProductos: function() {
+      fetch('http://localhost:8080/Apiweb/v1/producto/')
+        .then(response => response.json())
+        .then(datos => {
+          const productGrid = document.querySelector('.product-grid');
+          productGrid.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos productos
+          datos.forEach(producto => {
+            const productoElement = document.createElement('div');
+            productoElement.classList.add('producto');
+            productoElement.innerHTML = `
+              <img src="${producto.image_Url}" alt="${producto.nombre}">
+              <h3>${producto.nombre}</h3>
+              <p>${producto.descripcion}</p>
+              <p>Precio: ${producto.precio}</p>
+            `;
+            productGrid.appendChild(productoElement);
+          });
+        })
+        .catch(error => console.error('Error:', error));
     },
-    body: JSON.stringify(productoDTO)
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error al crear el producto');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Producto creado con éxito:', data);
-  })
-  .catch(error => {
-    console.error('Error al crear el producto:', error);
-  });
-}
   
-//Metodo para actualizar el producto de la base de datos
-function actualizarProductoPorId(productoId, detallesProducto) {
-  fetch(`http://localhost:8080/Apiweb/v1/producto/${productoId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
+    crearProducto: function(productoDTO) {
+      fetch('http://localhost:8080/Apiweb/v1/producto/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(productoDTO)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al crear el producto');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Producto creado con éxito:', data);
+      })
+      .catch(error => {
+        console.error('Error al crear el producto:', error);
+      });
     },
-    body: JSON.stringify(detallesProducto)
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error al actualizar el producto');
+  
+    actualizarProductoPorId: function(productoId, detallesProducto) {
+      fetch(`http://localhost:8080/Apiweb/v1/producto/${productoId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(detallesProducto)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al actualizar el producto');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Producto actualizado con éxito:', data);
+      })
+      .catch(error => {
+        console.error('Error al actualizar el producto:', error);
+      });
+    },
+  
+    obtenerProductoPorId: function(productoId) {
+      fetch(`http://localhost:8080/Apiweb/v1/producto/${productoId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al obtener el producto');
+        }
+        return response.json();
+      })
+      .then(producto => {
+        console.log('Producto:', producto);
+      })
+      .catch(error => {
+        console.error('Error al obtener el producto:', error);
+      });
+    },
+  
+    filtrarPorGenero: function(genero) {
+      fetch(`http://localhost:8080/Apiweb/v1/producto/${genero}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al filtrar productos por género');
+        }
+        return response.json();
+      })
+      .then(productos => {
+        console.log('Productos filtrados por género:', productos);
+      })
+      .catch(error => {
+        console.error('Error al filtrar productos por género:', error);
+      });
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Producto actualizado con éxito:', data);
-  })
-  .catch(error => {
-    console.error('Error al actualizar el producto:', error);
-  });
-}
-
-//Metodo para obtener un producto por el id del producto
-function obtenerProductoPorId(productoId) {
-  fetch(`http://localhost:8080/Apiweb/v1/producto/${productoId}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('No se pudo obtener el producto');
-    }
-    return response.json();
-  })
-  .then(producto => {
-    console.log('Producto obtenido:', producto);
-  })
-  .catch(error => {
-    console.error('Error al obtener el producto:', error);
-  });
-}
-
-//Metodo para filtrar los productos por genero
-function filtrarProductosPorGenero(genero) {
-  fetch(`http://localhost:8080/Apiweb/v1/producto/${genero}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('No se pudo filtrar por género');
-    }
-    return response.json();
-  })
-  .then(productos => {
-    console.log(`Productos filtrados por género ${genero}:`, productos);
-  })
-  .catch(error => {
-    console.error('Error al filtrar productos por género:', error);
-  });
-}
+};
+  
+export default productoController;
+  
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,25 @@
+import productoController from "./functions";
 //Logica de los botones y de mas elementos que no son directamente metodos
+
+//Metodo que muestra los productos en la pagina apenas abra
+document.addEventListener('DOMContentLoaded', function() {
+  obtenerProductos();
+});
+
+// Obtener referencia a los elementos de género
+const hombresBtn = document.querySelector('.dropdown-content a:nth-child(1)');
+const mujeresBtn = document.querySelector('.dropdown-content a:nth-child(2)');
+const niñosBtn = document.querySelector('.dropdown-content a:nth-child(3)');
+
+// Agregar eventos de clic a los botones de género
+hombresBtn.addEventListener('click', () => {
+  productoController.filtrarPorGenero('HOMBRE');
+});
+
+mujeresBtn.addEventListener('click', () => {
+  productoController.filtrarPorGenero('MUJER');
+});
+
+niñosBtn.addEventListener('click', () => {
+  productoController.filtrarPorGenero('NIÑO');
+});
