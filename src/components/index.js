55d852de2f39bae54d2b3087466860c8068b3d40
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-18 21:34:45 -0500

    Cree
    Carpeta de funciones

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,1 @@
-fetch('/Apiweb/productos')
-  .then(response => response.json())
-  .then(datos => {
-    const productGrid = document.querySelector('.product-grid');
-    datos.forEach(producto => {
-      const productoElement = document.createElement('div');
-      productoElement.classList.add('producto');
-      productoElement.innerHTML = `
-        <img src="${producto.imagen}" alt="${producto.nombre}">
-        <h3>${producto.nombre}</h3>
-        <p>${producto.descripcion}</p>
-        <p>Precio: ${producto.precio}</p>
-      `;
-      productGrid.appendChild(productoElement);
-    });
-  })
-  .catch(error => console.error('Error:', error));
+//Logica de los botones y de mas elementos que no son directamente metodos
