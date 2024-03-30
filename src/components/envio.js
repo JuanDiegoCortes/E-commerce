e12541714ca7ec7614d12b4da5ad63a1a6c4a4f3
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-03-30 12:44:21 -0500

    Envio

--- src/components/envio.js
+++ src/components/envio.js
@@ -22,6 +22,7 @@ document.querySelectorAll('.boton-modalidadEntrega').forEach(button => {
 document.getElementById("botonPago").addEventListener("click", capturarValores);
 
 function capturarValores() {
+
     // nombre = document.getElementById('nombre').value;
     // apellido = document.getElementById('apellido').value;
     telefono = document.getElementById('numTelefono').value;
@@ -29,37 +30,40 @@ function capturarValores() {
     // codigoPostal = document.getElementById('codigoPostal').value;
     // referencias = document.getElementById('referencias').value;
 
-    const envioData = {
+    if (ciudadSeleccionadaId === "" || modalidadEntregaSeleccionada === "" || telefono === "" || direccion === "") {
+        alert('Por favor, complete todos los campos.');
+        return console.log(ciudadSeleccionadaId, modalidadEntregaSeleccionada, telefono, direccion)
+    }else {
+        const envioData = {
 
-        // apellido: apellido,
-        direccion: direccion,
-        modalidad: modalidadEntregaSeleccionada,
-        telefono: telefono,
-        idCiudad: {idCiudad:ciudadSeleccionadaId}
-        // nombre: nombre
-        // codigoPostal: codigoPostal,
-        // referencias: referencias,
-    };
-    console.log(envioData);
-    // enviarDatos(envioData);
+            // apellido: apellido,
+            direccion: direccion,
+            modalidadEntrega: modalidadEntregaSeleccionada,
+            telefono: telefono,
+            idCiudad: {idCiudad:ciudadSeleccionadaId}
+            // nombre: nombre
+            // codigoPostal: codigoPostal,
+            // referencias: referencias,
+        };
+        console.log(envioData);
+        enviarDatos(envioData);
+    }
 }
 
-// function enviarDatos(data){
-//     fetch(`http://localhost:8081/Apiweb/v1/envio/`, {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//     .then(response => {
-//         if (response.ok) {
-//             console.log('Datos enviados correctamente.');
-//         } else {
-//             console.error('Error al enviar los datos.');
-//         }
-//     })
-//     .catch(error => {
-//         console.error('Error en la solicitud Fetch:', error);
-//     });
-// }
+function enviarDatos(data){
+    fetch("http://localhost:8081/Apiweb/v1/envio/", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Datos enviados correctamente.');
+            return response.json();
+        } else {
+            alert('Error al enviar los datos.');
+        }
+    })
+}
