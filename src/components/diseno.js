4d5e1ba4556bbe20e13fe7977e206bf5ea7dbfd9
ohborr <juan.borrero03@gmail.com>
2024-04-06 20:44:23 -0500

    Update diseno.js

--- src/components/diseno.js
+++ src/components/diseno.js
@@ -1,6 +1,6 @@
 const designs = [
-    { id: 1, name: 'Camiseta estampada', price: 25.99, image: '/src/assets' },
-    { id: 2, name: 'Vestido floral', price: 49.99, image: '/src/assets/img/...' },
+    { id: 1, name: 'XXXX', price: 500000, image: '/src/assets/img/...' },
+    { id: 2, name: 'XXXX', price: 1000000, image: '/src/assets/img/...' },
     // ...otros diseños
 ];
 
@@ -9,8 +9,8 @@ function listarDiseños() {
     designs.forEach(design => {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
-            <img src="${'/src/assets/img/disenoP/01.jpg'}" alt="${'disenoP'}">
-            <h3>${'disenoP'}</h3>
+            <img src="${'/src/assets/img/disenoP/01.jpg'}" alt="${design.name}">
+            <h3>${design.name}</h3>
             <p>Precio: $${design.price.toFixed(2)}</p>
         `;
         designList.appendChild(listItem);
