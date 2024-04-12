42b5c76ea9219881dafc79c954b95e303e89f6aa
Juan Diego <juancortesmur20@gmail.com>
2024-04-11 22:07:15 -0500

    Orden detalle Por el lado de Envio ya esta, ahora necesitamos los productos y tallas

--- src/components/detalleOrden.js
+++ src/components/detalleOrden.js
@@ -1,10 +1,19 @@
 window.onload = function() {
     const ordenSeleccionada = JSON.parse(sessionStorage.getItem("ordenSeleccionada"));
-
     if (ordenSeleccionada){
-        document.getElementById("nombreOrden").innerText +=`\t ${ordenSeleccionada.cedula.nombre}`;
-        document.getElementById("correoOrden").innerText += ordenSeleccionada.cedula.correo;
-        document.getElementById("cedulaOrden").innerText += ordenSeleccionada.cedula.cedula;
+        document.getElementById("nombreOrden").innerText += ordenSeleccionada.cedula.nombre;
+        document.getElementById("apellidoOrden").innerText += ordenSeleccionada.idEnvio.apellido;
+        document.getElementById("telefonoOrden").innerText += ordenSeleccionada.idEnvio.telefono;
+        document.getElementById("direccionOrden").innerText += ordenSeleccionada.idEnvio.direccion;
+        document.getElementById("codigo-postalOrden").innerText += ordenSeleccionada.idEnvio.codigoPostal;
+        document.getElementById("ciudadOrden").innerText += ordenSeleccionada.idEnvio.idCiudad.nombre;
+        document.getElementById("modalidad-entregaOrden").innerText += ordenSeleccionada.idEnvio.modalidadEntrega;
+        document.getElementById("fechaOrden").innerText += ordenSeleccionada.fecha;
+        if(ordenSeleccionada.idEnvio.referencias === null){
+            document.getElementById("referenciasOrden").innerText += "No hay referencias";
+        }else{
+            document.getElementById("referenciasOrden").innerText += ordenSeleccionada.idEnvio.referencias;
+        }
     }else{
         console.error("No se encontró ninguna orden seleccionada.");
     }
