009d3d4e8b64fca3e1a059ec408c16fcd13bcbd6
JNarvaez712 <narvaez.jonathan0712@gmail.com>
2024-05-21 20:39:13 -0500

    avances en gestionar y editar categorias

--- src/components/gestionarCategorias.js
+++ src/components/gestionarCategorias.js
@@ -1,55 +1,47 @@
 window.onload = function() {
     sessionStorage.removeItem("productoId");
 
-    let productos = [];
+    let categorias = [];
     
     let btnsEditar = document.querySelectorAll(".btn-editar");
 
     const inputFiltro = document.querySelector(".input-search")
-    const botonBuscarID = document.querySelector(".button-search-id")
+    const botonBuscarId = document.querySelector(".button-search-id")
     const botonBuscarNombre = document.querySelector(".button-search-nombre")
-    const containerProductos = document.querySelector(".contenedor-productos");
+    const containerCategorias = document.querySelector(".contenedor-categorias");
 
     const url = 'http://localhost:8081/Apiweb/v1/producto/';
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        productos = data;
-        mostrarProductos(productos);
+        categorias = data;
+        mostrarCategorias(categorias);
         });
 
-    function mostrarProductos(productos) {
-        containerProductos.innerHTML = "";
+    function mostrarCategorias(categorias) {
+        containerCategorias.innerHTML = "";
 
         const trHeader = document.createElement("tr");
         trHeader.innerHTML = `
-            <th>Imagen</th>
+            <th>ID</th>
             <th>Nombre</th>
             <th>Descripción</th>
-            <th>Estado</th>
-            <th>Personalizable</th>
-            <th>Género</th>
+            <th>Id subcategorias</th>
+            <th>Editar</th>
         `;
-        containerProductos.appendChild(trHeader);
+        containerCategorias.appendChild(trHeader);
 
-        productos.forEach(producto => {
+        categorias.forEach(categoria => {
             const tr = document.createElement("tr");
-            let tallas = producto.idProdTalla.map(talla => talla.idTalla).join(", ");
             tr.innerHTML = `
-            <td><img src="${producto.image_Url}" alt="${producto.nombre}"></td>
-            <td>${producto.idProducto}</td>
-            <td>${producto.nombre}</td>
-            <td>${producto.descripcion}</td>
-            <td>${producto.precio}</td>
-            <td>${producto.estado}</td>
-            <td>${producto.personalizable}</td>  
-            <td>${producto.genero}</td>
-            <td>${producto.idCategoria.nombre}</td>
-            <td>${tallas}</td>
-            <td><button class="btn-gestionar" id="${producto.idProducto}">Editar</button></td>
+            <td>${categoria.idCategoria}</td>
+            <td>${categoria.nombre}</td>
+            <td>${categoria.descripcion}</td>
+            <td>${categoria.subIdCategoria}</td>
+            <td><button class="btn-editar" id="${categoria.idCategoria}">Editar</button></td>
             `;
 
-            containerProductos.appendChild(tr);
+            containerCategorias.appendChild(tr);
         });
         actualizarBotonesEditar();
     }    
@@ -59,32 +51,32 @@ window.onload = function() {
     
         btnsEditar.forEach(boton => {
             boton.addEventListener("click", () => {
-                sessionStorage.setItem("productoId", JSON.stringify(parseInt(boton.id)))
-                window.location.href = "editarProducto.html";
+                sessionStorage.setItem("idCategoria", JSON.stringify(parseInt(boton.id)))
+                window.location.href = "editarCategoria.html";
             });
         });
     }
 
     function addEventListeners() {
-        botonBuscarID.addEventListener('click', function() {
+        botonBuscarId.addEventListener('click', function() {
             const idBuscado = parseInt(inputFiltro.value);
-            const productoFiltrado = productos.find(producto => producto.idProducto === idBuscado);
+            const categoriasFiltradas = categorias.find(categoria => categoria.idCategoria === idBuscado);
         
-            if (productoFiltrado) {
-                mostrarProductos([productoFiltrado]);
+            if (categoriasFiltradas) {
+                mostrarCategorias([categoriasFiltradas]);
             } else {
-                containerProductos.innerHTML = "No se encontró ningún producto con ese ID.";
+                containerCategorias.innerHTML = "No se encontró ningún producto con ese ID.";
             }
         });
 
         botonBuscarNombre.addEventListener('click', function() {
             const nombreBuscado = inputFiltro.value.toLowerCase();
-            let productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase() === nombreBuscado);
+            let categoriasFiltradas = categorias.filter(categoria => categoria.nombre.toLowerCase() === nombreBuscado);
 
-            if (productosFiltrados.length > 0) {
-                mostrarProductos(productosFiltrados);
+            if (categoriasFiltradas.length > 0) {
+                mostrarCategorias(categoriasFiltradas);
             } else {
-                containerProductos.innerHTML = "No se encontró ningún producto con ese nombre";
+                containerCategorias.innerHTML = "No se encontró ningún producto con ese nombre";
             }
         });
     }
